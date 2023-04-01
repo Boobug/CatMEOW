e51a9113100bc38000fb807e8262e59fc7409046
Ashal <shaulca123@gmail.com>
2023-04-01 12:58:28 +0300

    no longer included in the bot but still there for inspiration

--- cogs/fun.py
+++ cogs/fun.py
@@ -103,30 +103,6 @@ class Fun(commands.Cog, name="fun"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="randomfact", description="Get a random fact.")
-    @checks.not_blacklisted()
-    async def randomfact(self, context: Context) -> None:
-        """
-        Get a random fact.
-
-        :param context: The hybrid command context.
-        """
-        # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://uselessfacts.jsph.pl/random.json?language=en"
-            ) as request:
-                if request.status == 200:
-                    data = await request.json()
-                    embed = discord.Embed(description=data["text"], color=0xD75BF4)
-                else:
-                    embed = discord.Embed(
-                        title="Error!",
-                        description="There is something wrong with the API, please try again later",
-                        color=0xE02B2B,
-                    )
-                await context.send(embed=embed)
-
     @commands.hybrid_command(
         name="coinflip", description="Make a coin flip, but give your bet before."
     )
@@ -168,5 +144,8 @@ class Fun(commands.Cog, name="fun"):
         await context.send("Please make your choice", view=view)
 
 
-async def setup(bot):
-    await bot.add_cog(Fun(bot))
+# uncomment the following lines to include Fun
+# async def setup(bot):
+#     await bot.add_cog(Fun(bot))
+
+# keep this file for inspiration, there are cool discord feature use it.
