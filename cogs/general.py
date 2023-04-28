7ab31edfee7a874ce98dc66963b5dcc43dea07ec
Ashal <shaulca123@gmail.com>
2023-04-28 09:18:58 +0300

    fixing my accidental deletion of my changes

--- cogs/general.py
+++ cogs/general.py
@@ -19,9 +19,7 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
-from exceptions import UserNotOwner
 from helpers import checks
-
 
 class General(commands.Cog, name="general"):
     def __init__(self, bot):
@@ -40,9 +38,9 @@ class General(commands.Cog, name="general"):
             if not await checks.is_owner_simple(context) and (cog_name == "owner" or cog_name == "moderation"):
                 continue
             cog = self.bot.get_cog(cog_name.lower())
-            commands = cog.get_commands()
+            cog_commands = cog.get_commands()
             data = []
-            for command in commands:
+            for command in cog_commands:
                 description = command.description.partition("\n")[0]
                 data.append(f"{prefix}{command.name} - {description}")
             help_text = "\n".join(data)
@@ -76,6 +74,36 @@ class General(commands.Cog, name="general"):
             inline=False,
         )
         embed.set_footer(text=f"Requested by {context.author}")
+        await context.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="serverinfo",
+        description="Get some useful (or not) information about the server.",
+    )
+    @checks.not_blacklisted()
+    async def serverinfo(self, context: Context) -> None:
+        """
+        Get some useful (or not) information about the server.
+        :param context: The hybrid command context.
+        """
+        roles = [role.name for role in context.guild.roles]
+        if len(roles) > 50:
+            roles = roles[:50]
+            roles.append(f">>>> Displaying[50/{len(roles)}] Roles")
+        roles = ", ".join(roles)
+
+        embed = discord.Embed(
+            title="**Server Name:**", description=f"{context.guild}", color=0x9C84EF
+        )
+        if context.guild.icon is not None:
+            embed.set_thumbnail(url=context.guild.icon.url)
+        embed.add_field(name="Server ID", value=context.guild.id)
+        embed.add_field(name="Member Count", value=context.guild.member_count)
+        embed.add_field(
+            name="Text/Voice Channels", value=f"{len(context.guild.channels)}"
+        )
+        embed.add_field(name=f"Roles ({len(context.guild.roles)})", value=roles)
+        embed.set_footer(text=f"Created at: {context.guild.created_at}")
         await context.send(embed=embed)
 
     @commands.hybrid_command(
